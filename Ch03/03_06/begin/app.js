e8d9d9cfd0248a04f4ca021fde82c571c9125aa8
Dat T. Tran <datttrian@gmail.com>
2023-08-18 15:57:31 -0400

    Solution: Create classes

--- Ch03/03_06/begin/app.js
+++ Ch03/03_06/begin/app.js
@@ -1,42 +1,47 @@
-'use strict';
+"use strict";
 
 const newMember = [
   84236,
-  'Luis',
-  'Rubio',
-  '25624 Main St.',
-  'Pawtucket',
-  'Rhode Island',
-  '02906',
+  "Luis",
+  "Rubio",
+  "25624 Main St.",
+  "Pawtucket",
+  "Rhode Island",
+  "02906",
 ];
 
-class label {
+class Label {
   constructor(options) {
     this.number = options.number;
   }
   addFullName() {
-    this.fullName = newMember[1] + ' ' + newMember[2];
+    this.fullName = newMember[1] + " " + newMember[2];
+    return this;
   }
   addStreet() {
     this.street = newMember[3];
+    return this;
   }
   addCSZ() {
-    this.csz = newMember[4] + ' ' + newMember[5] + ', ' + newMember[6];
+    this.csz = newMember[4] + " " + newMember[5] + ", " + newMember[6];
+    return this;
   }
   logLabel() {
-    const that = this;
-    return function() {
-      console.log('\n',that.fullName,'\n',that.street,'\n',that.csz);
-    }
+    console.log("\n", this.fullName, "\n", this.street, "\n", this.csz);
+    // const that = this;
+    // return function () {
+    //   console.log("\n", that.fullName, "\n", that.street, "\n", that.csz);
+    // };
   }
 }
 
-const currentLabel = new label({
+const currentLabel = new Label({
   number: newMember[0],
 });
 
-currentLabel.addFullName();
-currentLabel.addStreet();
-currentLabel.addCSZ();
+// currentLabel.addFullName();
+// currentLabel.addStreet();
+// currentLabel.addCSZ();
+currentLabel.addFullName().addStreet().addCSZ();
 
-window.addEventListener('load', currentLabel.logLabel());
+window.addEventListener("load", currentLabel.logLabel.call(currentLabel));
