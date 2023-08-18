f93509fce90c2ddad0711764369271d1c926325a
Dat T. Tran <datttrian@gmail.com>
2023-08-18 17:29:38 -0400

    Avoid unary increments and decrements

--- Ch06/06_04/begin/app.js
+++ Ch06/06_04/begin/app.js
@@ -1,7 +1,13 @@
-'use strict';
+"use strict";
 
 let guess = 28;
 
-console.log(guess++);
-console.log(++guess);
+// console.log(guess++);
+// console.log(++guess);
+// console.log(guess);
+
+console.log(guess);
+guess += 1;
+guess += 1;
+console.log(guess);
 console.log(guess);
