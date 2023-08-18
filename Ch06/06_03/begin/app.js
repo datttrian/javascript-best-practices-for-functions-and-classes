fdb3aacdaf53938ec0e952935bb94913e9c3c523
Dat T. Tran <datttrian@gmail.com>
2023-08-18 17:27:52 -0400

    Avoid with

--- Ch06/06_03/begin/app.js
+++ Ch06/06_03/begin/app.js
@@ -1,10 +1,12 @@
-'use strict';
+"use strict";
 
-let a;
+// let a;
 const r = 10;
 
-with (Math) {
-  a = PI * pow(r, 2);
-}
+// with (Math) {
+//   a = PI * pow(r, 2);
+// }
+
+const a = Math.PI * Math.pow(r, 2);
 
 console.log(a);
