fdee3fb264c4990fcf01b793d88c130289a3fde2
Dat T. Tran <datttrian@gmail.com>
2023-08-18 16:42:53 -0400

    Use case to indicate a module's default export type

--- Ch04/04_03/begin/app.js
+++ Ch04/04_03/begin/app.js
@@ -1,21 +1,18 @@
-import distanceTo from './modules/distanceTo.js';
+import distanceTo from "./modules/distanceTo.js";
+import TestLocation from "./modules/TestLocation.js";
 
 const userPrefs = {
-  city: 'Chicago',
+  city: "Chicago",
   lat: 41.878113,
   lon: -87.629799,
 };
 
-const testLocation = {
-  city: 'Alexandria',
-  lat: 31.2156400,
-  lon: 29.9552700,
-};
-
-console.log(distanceTo(
-  userPrefs.lat, 
-  userPrefs.lon, 
-  testLocation.lat,
-  testLocation.lon, 
-  'M')
+console.log(
+  distanceTo(
+    userPrefs.lat,
+    userPrefs.lon,
+    TestLocation.lat,
+    TestLocation.lon,
+    "M"
+  )
 );
---
+++ Ch04/04_03/begin/modules/TestLocation.js
@@ -0,0 +1,7 @@
+const TestLocation = {
+  city: "Alexandria",
+  lat: 31.21564,
+  lon: 29.95527,
+};
+
+export default TestLocation;
