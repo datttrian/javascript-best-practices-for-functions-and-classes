78bfe9bec04863d58f01291623ab73c144a2179e
Dat T. Tran <datttrian@gmail.com>
2023-08-18 15:06:14 -0400

    Use arrow syntax for anonymous functions

--- Ch02/02_04/begin/.eslintrc.js
+++ Ch02/02_04/begin/.eslintrc.js
@@ -1,13 +1,14 @@
-'use strict';
+"use strict";
 
 module.exports = {
   env: {
     es6: true,
   },
   rules: {
-    strict: ['error', 'global'],
-    'func-style': ['error', 'expression'],
-    'no-new-func': 'error',
-    'no-param-reassign': 'error',
+    strict: ["error", "global"],
+    "func-style": ["error", "expression"],
+    "no-new-func": "error",
+    "no-param-reassign": "error",
+    "prefer-arrow-callback": "error",
   },
 };
