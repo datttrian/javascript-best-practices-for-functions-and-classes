81804373390608d654c2763ecc5f230caf47f5a0
Dat T. Tran <datttrian@gmail.com>
2023-08-18 16:33:45 -0400

    Configure module settings in ESLint

--- Ch04/04_01/begin/.eslintrc.js
+++ Ch04/04_01/begin/.eslintrc.js
@@ -1,18 +1,19 @@
-'use strict';
-
 module.exports = {
   env: {
     es6: true,
   },
+  parserOptions: {
+    sourceType: "module",
+  },
   rules: {
-    strict: ['error', 'global'],
-    'func-style': ['error', 'expression'],
-    'no-new-func': 'error',
-    'no-param-reassign': 'error',
-    'prefer-arrow-callback': 'error',
-    'arrow-parens': ['error', 'always'],
-    'arrow-body-style': ['error', 'as-needed'],
-    'new-cap': 'error',
-    'no-invalid-this': 'error',
+    strict: ["error", "global"],
+    "func-style": ["error", "expression"],
+    "no-new-func": "error",
+    "no-param-reassign": "error",
+    "prefer-arrow-callback": "error",
+    "arrow-parens": ["error", "always"],
+    "arrow-body-style": ["error", "as-needed"],
+    "new-cap": "error",
+    "no-invalid-this": "error",
   },
 };
--- Ch04/04_01/begin/app.js
+++ Ch04/04_01/begin/app.js
@@ -1,23 +1,23 @@
-'use strict';
-
-import distanceTo from './modules/distanceTo.js';
+import distanceTo from "./modules/distanceTo.js";
 
 const userPrefs = {
-  city: 'Chicago',
+  city: "Chicago",
   lat: 41.878113,
   lon: -87.629799,
 };
 
 const testLocation = {
-  city: 'Alexandria',
-  lat: 31.2156400,
-  lon: 29.9552700,
+  city: "Alexandria",
+  lat: 31.21564,
+  lon: 29.95527,
 };
 
-console.log(distanceTo(
-  userPrefs.lat, 
-  userPrefs.lon, 
-  testLocation.lat,
-  testLocation.lon, 
-  'M')
+console.log(
+  distanceTo(
+    userPrefs.lat,
+    userPrefs.lon,
+    testLocation.lat,
+    testLocation.lon,
+    "M"
+  )
 );
